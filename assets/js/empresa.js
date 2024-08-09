97817b73438427cab8ea52af5c8c6d83f528735f
walterpacheco <walter.e.pacheco.chandia@gmail.com>
2024-08-08 21:27:32 -0400

    Se corrigen errores

--- assets/js/empresa.js
+++ assets/js/empresa.js
@@ -1,31 +1,31 @@
 export class Empresa{
-constructor(idEmpresa, rut, nombre) {
-    this.idEmpresa = idEmpresa;
-    this.rut = rut;
-}
-}
-agregarImportaciones(Importaciones) {
+    constructor(idEmpresa, rut, nombre) {
+        this.idEmpresa = idEmpresa;
+        this.rut = rut;
+    }
+    }
+    agregarImportaciones(Importaciones) {
+        this.importacion.push(importacion);
+    }
     
-}
-
-export class Importacion extends Empresa {
-
-constructor (idProducto, nombreProducto, numeroProducto, precioProducto ){
-    this.idProducto = idProducto;
-    this.nombreProducto = nombreProducto;
-    this.numeroProducto = numeroProducto;
-    this.precioProducto = precioProducto;
-}
-
-calcularPrecioindividual {
-    return this.numeroProducto * this.precioProducto;
-}
-
-
-
-calcularTotalImportaciones () {
+    export class Importacion extends Empresa {
     
-}
-
-
-}
+    constructor (idProducto, nombreProducto, numeroProducto, precioProducto ){
+        this.idProducto = idProducto;
+        this.nombreProducto = nombreProducto;
+        this.numeroProducto = numeroProducto;
+        this.precioProducto = precioProducto;
+    }
+    
+    calcularPrecio() {
+        return this.numeroProducto * this.precioProducto;
+    }
+    calcularTotalImportaciones () {
+        return this.Importaciones
+    }
+    
+    
+    
+    
+    
+    }
